badc1db10cf5d64bac114c58abf106f188463d27
Bohdan_Novytskyi <gruvoz.1234@gmail.com>
2025-03-13 12:02:51 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import keyword
+import string
+
+test_list = ["get_value"]
+
+is_valid = True
+
+for name in test_list:
+    if " " in name:
+        is_valid = False
+        break
+    elif name in keyword.kwlist:
+        is_valid = False
+        break
+    elif name.count('_') > 1:
+        is_valid = False
+        break
+    elif name[0].isdigit():
+        is_valid = False
+        break
+    for upper_symbol in name:
+        if upper_symbol.isupper():
+            is_valid = False
+            break
+    for punctuation_symbol in name:
+        if punctuation_symbol in string.punctuation and punctuation_symbol != "_":
+            is_valid = False
+            break
+
+
+print(is_valid)
